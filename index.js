816df7cd37ec0fbb680f33b763d741d5eb0e5373
Enkhjil Enkhbaatar <enkhjil.eb@gmail.com>
2022-10-20 02:14:23 +0900

    fix: add custom sox recorder

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ class Recording {
 
     this.options = Object.assign(defaults, options)
 
-    const recorder = recorders.load(this.options.recorder)
-    const { cmd, args, spawnOptions = {} } = recorder(this.options)
+    // const recorder = recorders.load(this.options.recorder)
+    // const { cmd, args, spawnOptions = {} } = recorder(this.options)
+    const { cmd, args, spawnOptions = {} } = customRecorder(this.options)
 
     this.cmd = cmd
     this.args = args
@@ -103,6 +104,37 @@ Enable debugging with the environment variable DEBUG=record.`
     return this._stream
   }
 }
+
+function customRecorder(options) {
+  const cmd = options.cmd
+
+  let args = [
+    '--default-device',
+    '--no-show-progress', // show no progress
+    '--rate', options.sampleRate, // sample rate
+    '--channels', options.channels, // channels
+    '--encoding', 'signed-integer', // sample encoding
+    '--bits', '16', // precision (bits)
+    '--type', options.audioType, // audio type
+    '-' // pipe
+  ]
+
+  if (options.endOnSilence) {
+    args = args.concat([
+      'silence', '1', '0.1', options.thresholdStart || options.threshold + '%',
+      '1', options.silence, options.thresholdEnd || options.threshold + '%'
+    ])
+  }
+
+  const spawnOptions = { }
+
+  if (options.device) {
+    spawnOptions.env = { ...process.env, AUDIODEV: options.device }
+  }
+
+  return { cmd, args, spawnOptions }
+}
+
 
 module.exports = {
   record: (...args) => new Recording(...args)
